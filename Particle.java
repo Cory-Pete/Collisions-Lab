a5fc97f11b38c187784069d6e8296d092a4d7bd7
S-Lyle <sarahkathrynlyle@gmail.com>
2017-01-27 14:14:23 -0700

    Update Particle.java

--- Particle.java
+++ Particle.java
@@ -14,6 +14,9 @@ public class Particle {
     int r;
     int g;
     int b;
+    int count=0;
+    private static final double INFINITY = Double.POSITIVE_INFINITY;
+
 
     public Particle(double X, double Y, double vX, double vY, double radius, double mass, int r, int g, int b){ //position and velocity
         this.X=X;
@@ -77,33 +80,8 @@ public class Particle {
     public double collidesX(double t){//returns when particle collides with a wall vertically
         double currentT=t;
         double dt=-1;
-        /*
-        if (vX==0) return -1;
-        else {
-            *//*
-            have to make it solve for time not if the coordinates are equal. otherwise cant sort for time. also the program
-            already goes through a time loop, so adding another isnt needed.
-            *//*
-
-            //x+v(t+change in t)= border vertical
-           //((border vertical-x)/v))-t=change in t //does actual t matter? perhaps in solving for border coor
-
-            for(double time=0; time<=.5; time+=0.0001){//cycling through 1 second at 0.01 time. will i miss some?
-               if(vX>1) {
-                   if ((((1 - X) / vX)) - currentT == time) {
-                       return time;
-                   }
-               }
-               if(vX<1){
-                   if ((((0 - X) / vX)) - currentT == time) {
-                       return time;
-                   }
-               }
-            }
-        }
-        return -1; //made it out of loop without finding anything*/
-
-        if (vX==0) return -1;
+        
+        if (vX==0) return INFINITY;
         else if(vX>0) {
            dt= ((1-radius-X)/vX)-currentT;
         }
@@ -116,7 +94,7 @@ public class Particle {
     public double collidesY(double t){//returns when particle collides with a wall vertically
         double currentT=t;
         double dt=-1;
-        if (vY==0) return -1;
+        if (vY==0) return INFINITY;
        else if(vY>0) {
             dt= ((1-radius-Y)/vY)-currentT;
         }
@@ -125,4 +103,58 @@ public class Particle {
         }
         return dt;
     }
+    public void bounceOff(Particle that) {
+        double dx  = that.X - this.X;
+        double dy  = that.Y - this.Y;
+        double dvx = that.vX - this.vY;
+        double dvy = that.vY - this.vY;
+        double dvdr = dx*dvx + dy*dvy;             // dv dot dr
+        double dist = this.radius + that.radius;   // distance between particle centers at collison
+
+        // magnitude of normal force
+        double magnitude = 2 * this.mass * that.mass * dvdr / ((this.mass + that.mass) * dist);
+
+        // normal force, and in x and y directions
+        double fx = magnitude * dx / dist;
+        double fy = magnitude * dy / dist;
+
+        // update velocities according to normal force
+        this.vX += fx / this.mass;
+        this.vY += fy / this.mass;
+        that.vX= fx / that.mass;
+        that.vY= fy / that.mass;
+
+        // update collision counts
+        this.count++;
+        that.count++;
+    }
+    public double timeToHit(Particle that) {
+        if (this == that) return INFINITY;
+        double dx  = that.X - this.X;
+        double dy  = that.Y - this.Y;
+        double dvx = that.vX - this.vX;
+        double dvy = that.vY - this.vY;
+        double dvdr = dx*dvx + dy*dvy;
+        if (dvdr > 0) return INFINITY;
+        double dvdv = dvx*dvx + dvy*dvy;
+        double drdr = dx*dx + dy*dy;
+        double sigma = this.radius + that.radius;
+        double d = (dvdr*dvdr) - dvdv * (drdr - sigma*sigma);
+        // if (drdr < sigma*sigma) StdOut.println("overlapping particles");
+        if (d < 0) return INFINITY;
+        return -(dvdr + Math.sqrt(d)) / dvdv;
+    }
+    public void move(double dt) {
+        X += vX * dt;
+        Y += vY * dt;
+    }
+
+    /**
+     * Draws this particle to standard draw.
+     */
+    public void draw() {
+        StdDraw.setPenColor(r, g, b);
+        StdDraw.filledCircle(X, Y, radius);
+    }
+
 }
